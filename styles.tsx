b8e982714ea4815cc20bb900015619f1ca9f8a9d
Chloe Twilligear <82474339+cah-chloetwilligear@users.noreply.github.com>
2021-07-19 15:10:22 -0400

    Create styles.tsx

---
+++ styles.tsx
@@ -0,0 +1,18 @@
+// styled text input to match Square Iframe integration
+// saving for later when we inevitably add additional customer fields for card info
+  
+  const StyledInput = styled.input`
+      border-radius: 6px;
+      border-color: #d9d9d9;
+      width: 100%;
+      border-width: thin;
+      border-style: inset;
+      font-family: 'Square Market', Arial, sans-serif;
+      font-size: 16px;
+      justify-content: center;
+      position: relative;
+      display: flex;
+      height: 48px;
+      box-sizing: content-box;
+      padding-left: 12px;
+    `;
